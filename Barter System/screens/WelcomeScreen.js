6a00823696060a8ba1af9a5256190a0bb1e4e032
Harshikahundal123 <71327072+Harshikahundal123@users.noreply.github.com>
2021-06-21 11:49:46 +0700

    Add files via upload

---
+++ Barter System/screens/WelcomeScreen.js
@@ -0,0 +1,8 @@
+import {Modal,TextInput} from 'react '
+showModal = () => {
+    return (
+      <Modal> <TextInput> placeholder={'First Name'} </TextInput> 
+      <TextInput>placeholder={'Last Name'} </TextInput> 
+      <TextInput> placeholder = {'Phone Number '}</TextInput>
+      <TextInput> placeholder = {'Adress'} </TextInput> 
+      <TextInput> placeholder = </TextInput>  </Modal> }
